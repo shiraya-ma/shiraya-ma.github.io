e67393562be192ac741cd0583a7e5e0c82de54f7
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-17 10:27:06 +0900

    :gear: visualizerを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 'use strict';
 import { resolve } from 'path';
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './docs'
+    outDir: './docs',
+    rollupOptions: {
+      plugins: [
+        visualizer()
+      ]
+    }
   },
   plugins: [react()],
   resolve: {
