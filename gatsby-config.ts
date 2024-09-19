8eaae0e84c3b9fe0f6b859f67733b4b13b87ef6f
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-19 14:09:03 +0900

    :gear: urlを変更

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,23 +1,26 @@
+'use strict';
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `shiraya-ma.github.io`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://docs.shiraya.ma`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
+  // graphqlTypegen: true,
+  graphqlTypegen: false,
   plugins: [
     "gatsby-plugin-layout",
     "gatsby-plugin-postcss",
-    "gatsby-plugin-sitemap", {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        "icon": "src/images/icon.png"
-    }
-  }]
+    // "gatsby-plugin-sitemap", {
+    //   resolve: 'gatsby-plugin-manifest',
+    //     options: {
+    //       "icon": "src/images/icon.png"
+    //   }
+    // }
+  ]
 };
 
 export default config;
