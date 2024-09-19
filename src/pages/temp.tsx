3137ca3d7ff9105e66d34494b5149f9ea353c156
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-19 14:49:34 +0900

    :tada: 仮ページ作成

---
+++ src/pages/temp.tsx
@@ -0,0 +1,23 @@
+// Temp
+'use strict';
+import React from 'react';
+
+const Temp: React.FC<Temp.Props> = (props) => {
+    const {} = props;
+    
+    return (
+        <>
+            this is temp page.
+        </>
+    );
+};
+
+namespace Temp {
+    export type Props = {};
+};
+
+export {
+    Temp
+};
+
+export default Temp;
