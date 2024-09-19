8e5cfbe656029600693993f499634fd2d3219bde
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-19 11:59:47 +0900

    :gear: gatsby-plugin-layout追加

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,10 +9,13 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    "gatsby-plugin-layout",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-sitemap", {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
     }
   }]
 };
