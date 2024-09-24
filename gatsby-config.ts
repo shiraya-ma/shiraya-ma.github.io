3226bef3eabeec461f92a0515dce061c2fc95d8a
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-24 11:32:36 +0900

    :art: ファビコンを追加

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,8 +5,9 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `shiraya-ma.github.io`,
-    siteUrl: `https://docs.shiraya.ma`
+    siteUrl: `https://docs.shiraya.ma`,
   },
+  trailingSlash: 'never',
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
@@ -22,13 +23,12 @@ const config: GatsbyConfig = {
       }
     },
     "gatsby-plugin-layout",
-    "gatsby-plugin-postcss",
-    // "gatsby-plugin-sitemap", {
-    //   resolve: 'gatsby-plugin-manifest',
-    //     options: {
-    //       "icon": "src/images/icon.png"
-    //   }
-    // }
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //   },
+    // },
+    "gatsby-plugin-postcss"
   ]
 };
 
--- src/layouts/root-layout/root-layout.tsx
+++ src/layouts/root-layout/root-layout.tsx
@@ -8,64 +8,72 @@ import { MaiLink, MaiUIProvider } from '@shiraya-ma/mai-ui';
 import { navs } from '@/configs';
 
 import '@shiraya-ma/mai-ui/fonts.css';
+import { Helmet } from 'react-helmet';
 
 const RootLayout: React.FC<RootLayout.Props> = (props) => {
     const { children } = props;
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     
     return (
-        <MaiUIProvider navigate={ navigate }>
-            <Navbar
-            classNames={{
-                wrapper: 'w-screen max-w-[100ch]'
-            }}
-            isBordered
-            isMenuOpen={ isMenuOpen }
-            onMenuOpenChange={ setIsMenuOpen }>
-                <NavbarContent
-                className='flex md:hidden'>
-                    <NavbarItem
-                    className='flex size-16 justify-center'>
-                        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-                    </NavbarItem>
-                </NavbarContent>
-                
-                <NavbarContent
-                className='hidden md:flex'>
-                    { navs.map(nav => (
-                        <NavbarItem key={ nav.label }>
-                            <MaiLink
-                            href={ nav.href }
-                            className='text-xl font-logo text-[var(--nextui-foreground)]'>
-                                { nav.label }
-                            </MaiLink>
+        <>
+            <Helmet>
+                <link rel='icon' href='/favicon.ico'/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            </Helmet>
+
+            <MaiUIProvider navigate={ navigate }>
+                <Navbar
+                classNames={{
+                    wrapper: 'w-screen max-w-[100ch]'
+                }}
+                isBordered
+                isMenuOpen={ isMenuOpen }
+                onMenuOpenChange={ setIsMenuOpen }>
+                    <NavbarContent
+                    className='flex md:hidden'>
+                        <NavbarItem
+                        className='flex size-16 justify-center'>
+                            <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
                         </NavbarItem>
-                    ))}
-                </NavbarContent>
+                    </NavbarContent>
+                    
+                    <NavbarContent
+                    className='hidden md:flex'>
+                        { navs.map(nav => (
+                            <NavbarItem key={ nav.label }>
+                                <MaiLink
+                                href={ nav.href }
+                                className='text-xl font-logo text-[var(--nextui-foreground)]'>
+                                    { nav.label }
+                                </MaiLink>
+                            </NavbarItem>
+                        ))}
+                    </NavbarContent>
 
-                <NavbarMenu
-                className='overflow-y-hidden'>
-                    { navs.map(nav => (
-                        <NavbarMenuItem key={ nav.label }>
-                            <MaiLink
-                            href={ nav.href }
-                            className='text-2xl font-logo text-[var(--nextui-foreground)]'
-                            showAnchorIcon>
-                                { nav.label }
-                            </MaiLink>
-                        </NavbarMenuItem>
-                    ))}
-                </NavbarMenu>
-            </Navbar>
+                    <NavbarMenu
+                    className='overflow-y-hidden'>
+                        { navs.map(nav => (
+                            <NavbarMenuItem key={ nav.label }>
+                                <MaiLink
+                                href={ nav.href }
+                                className='text-2xl font-logo text-[var(--nextui-foreground)]'
+                                showAnchorIcon>
+                                    { nav.label }
+                                </MaiLink>
+                            </NavbarMenuItem>
+                        ))}
+                    </NavbarMenu>
+                </Navbar>
 
-            <div
-            className='flex flex-grow'>
-                <main
-                className='flex flex-col flex-grow mx-auto w-screen max-w-[100ch] gap-4'>
-                    { children }
-                </main>
-            </div>
-        </MaiUIProvider>
+                <div
+                className='flex flex-grow'>
+                    <main
+                    className='flex flex-col flex-grow mx-auto w-screen max-w-[100ch] gap-4'>
+                        { children }
+                    </main>
+                </div>
+            </MaiUIProvider>
+        </>
     );
 };
 
