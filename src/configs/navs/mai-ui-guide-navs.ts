3d0e2410ca0917d26bf2445f81e1334b04b265a2
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-25 10:39:57 +0900

    :pen: ガイドのナビゲーション作成

---
+++ src/configs/navs/mai-ui-guide-navs.ts
@@ -0,0 +1,8 @@
+'use strict';
+import type { NavProps } from './navs';
+
+export const maiUIGuideNavs: NavProps[] = [
+    { href: '/installation', label: 'Instrallation' },
+    { href: '/mai-color', label: 'MaiColor' },
+    { href: '/mai-font', label: 'MaiFont' },
+];
