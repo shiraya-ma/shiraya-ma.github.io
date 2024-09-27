686c4ce7bf15db499af841470503f1f274ff18f0
shiraya-ma <mai.shirayama@outlook.jp>
2024-09-27 16:35:53 +0900

    :pen: フォントのimportを削減

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 'use strict';
-import { maiFonts } from "@shiraya-ma/mai-ui";
 import { maiUIConfig } from "@shiraya-ma/mai-ui/setup";
 
 module.exports = maiUIConfig({
   content: [],
   theme: {
     fontFamily: {
-      ...maiFonts,
       logo: [ 'Robot Flex', 'sans-serif' ]
     }
   }
