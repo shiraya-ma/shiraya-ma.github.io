19d51f76d50e2a9b7945ad034d5d265101bb0046
Mai Shirayama <138377007+shiraya-ma@users.noreply.github.com>
2025-11-02 14:42:28 +0000

    :wrench: setup maiui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { MaiUIProvider } from '@shiraya-ma/mai-ui'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <MaiUIProvider>
+      <App />
+    </MaiUIProvider>
+  </StrictMode>
 )
