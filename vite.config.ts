fcb4dd00b3761484123c648ba93325f377ce1d94
Mai Shirayama <138377007+shiraya-ma@users.noreply.github.com>
2025-11-02 18:05:16 +0000

    :wrench: config build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       '@': '/src',
